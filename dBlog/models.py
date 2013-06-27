c322ca0cc6bed64d61773220bfb51b2d07f3dd22
Hana Lee <voyaging.hana@gmail.com>
2013-06-28 01:51:34 +0900

    url 패턴정의 및 화면 출력 구현

--- dBlog/models.py
+++ dBlog/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=40, null=False)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30, null=False)
+
+
 class Article(models.Model):
     title = models.CharField(max_length=80, null=False)
     content = models.TextField(null=False)
@@ -11,18 +19,13 @@ class Article(models.Model):
     tags = models.ManyToManyField(Tag)
     comments = models.PositiveSmallIntegerField(default=0, null=True)
 
-
-class Category(models.Model):
-    name = models.CharField(max_length=40, null=False)
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=30, null=False)
+    class Admin:
+        pass
 
 
 class Comment(models.Model):
     content = models.TextField(null=False)
-    email = models.CharField(null=True)
+    email = models.CharField(max_length=50, null=True)
     password = models.CharField(max_length=30, null=False)
     modEnabled = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
--- dBlog/views.py
+++ dBlog/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
+from dBlog.models import Article
+
+def index(request, page=1):
+    page_title = '블로그 글 목록 화면'
+    articles = Article.objects.all()
+    return HttpResponse('안녕? 이곳은 [%s] 입니다.' % page_title)
