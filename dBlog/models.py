16a644ae9c6638194cdd71bd338eed8884320d04
Hana Lee <voyaging.hana@gmail.com>
2013-06-27 23:56:16 +0900

    make djBlog

---
+++ dBlog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=80, null=False)
+    content = models.TextField(null=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    deleted = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag)
+    comments = models.PositiveSmallIntegerField(default=0, null=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40, null=False)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30, null=False)
+
+
+class Comment(models.Model):
+    content = models.TextField(null=False)
+    email = models.CharField(null=True)
+    password = models.CharField(max_length=30, null=False)
+    modEnabled = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    deleted = models.DateTimeField(auto_now=True)
+    article = models.ForeignKey(Article)
