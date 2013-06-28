18431f38f314ef937c1419ce6ac5043b6bc44fa5
Hana Lee <voyaging.hana@gmail.com>
2013-06-28 20:25:45 +0900

    블로그 글을 읽어 오는 소스 추가. 각 페이지별로 블로그 글별로 보여주는 방법 추가. 하나의 글을 보여주는 URL /blog/article/1 한페이지에 보여줄 글(3개) 전체 보여주기 /blog/page/1 zentus sqlite jdbc 드라이버 추가. pyCharm 에서 사용하는 datasource 에 블로그용 디비 연결 설정.

--- dBlog/views.py
+++ dBlog/views.py
@@ -4,26 +4,62 @@ from dBlog.models import Article
 from django.template import Context, loader
 
 def index(request, page = 1):
-    page = int(page)
+    """
+
+    :param request:
+    :param page:
+    :return: HttpResponse
+    """
+    page_no = int(page)
+    if page_no < 0 :
+        page_no = 1
+
+    article_count = Article.objects.all().count()
+    if page_no > article_count :
+        page_no = 1
+
     page_title = '블로그 글 목록 화면'
 
     per_page = 3
-    start_pos = (page - 1) * per_page
+    start_pos = (page_no - 1) * per_page
     end_pos = start_pos + per_page
 
-    articles = Article.objects.all().order_by('-created')[ start_pos : end_pos ]
+    articles = Article.objects.all().select_related().order_by('-created')[ start_pos : end_pos ]
 
     tpl = loader.get_template('list.html')
     ctx = Context({
         'page_title' : page_title,
         'articles' : articles,
-        'current_page' : page
+        'current_page' : page_no
     })
     return HttpResponse(tpl.render(ctx))
 
 def read(request, article_id = None):
+    """
+
+    :param request:
+    :param article_id:
+    :return: HttpResponse
+    """
     page_title = '블로그 글 읽기'
 
     current_article = Article.objects.get(id = int(article_id))
+    try:
+        prev_article = current_article.get_previous_by_created()
+    except:
+        prev_article = None
 
-    return HttpResponse('[%s]번째 글입니다. 제목 : [%s]' % (current_article.id, current_article.title.encode('utf-8')))
+    try :
+        next_article = current_article.get_next_by_created()
+    except :
+        next_article = None
+
+    tpl = loader.get_template('read.html')
+    ctx = Context({
+        'page_title' : page_title,
+        'current_article' : current_article,
+        'prev_article' : prev_article,
+        'next_article' : next_article
+    })
+
+    return HttpResponse(tpl.render(ctx))
