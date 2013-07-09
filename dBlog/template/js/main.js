86bad19043e0399030378eb2ca9cadce3ed1641b
Hana Lee <voyaging.hana@gmail.com>
2013-07-10 00:58:22 +0900

    상단 및 좌측, 메인 레이아웃 구성.

--- dBlog/template/js/main.js
+++ dBlog/template/js/main.js
@@ -1,1 +1,15 @@
+if (!window.djBlog) {
+    window.djBlog = {};
+}
 
+jQuery(document).ready(function() {
+    var $win_height = jQuery(window).height();
+    var $frame_height = jQuery(window).width() <= 767 ? $win_height : $win_height - 59;
+    jQuery('#iframe').height($frame_height);
+});
+
+jQuery(window).resize(function() {
+    var $wheight = jQuery(window).height();
+    var $fheight = jQuery(window).width() <= 767 ? $wheight : $wheight - 59;
+    jQuery('#iframe').height($fheight);
+});
